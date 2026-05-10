1620855497143a5d7ed21e10cbfdbf7e326de066
iasonas-evn <evangelou.i@live.unic.ac.cy>
2026-05-10 20:53:40 +0300

    fixed line 43. Declaration of the array total was swiatched to SIZE instead of 9.

--- DEBUGGINGSOFT/DEBUGGINGSOFT.cpp
+++ DEBUGGINGSOFT/DEBUGGINGSOFT.cpp
@@ -1,20 +1,176 @@
-// DEBUGGINGSOFT.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+/** \file bugExample.cpp
+ *  \brief     A little with logical bugs in it
+ *  \details   This program is littered with logical bugs. Your task is the
+ *              following:
+ *                1) Compile and run the program.
+ *                2) For each bug you find you must fix the bug then commit
+ *                   the code with a comment on the fix you did
+ *                3) There are a four functions that are not completed, you need to
+ *                   complete them and also update the menu and the main function
+ *  \author    Harald Gjermundrod
+ *  \version   0.1
+ *  \date      20/10/2025
+ *  \bug       Many logical bugs
+ *  \copyright University of Nicosia.
+ */
 
 #include <iostream>
+#include <cassert>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+// Function prototypes
+int printMenu();
+void fillInArray(int[], const int);
+void multArrays(const int[], const int[], int[], const int);
+void displayArray(const int[], const int);
+int sumOddArray(const int[], const int);
+bool isAllPositive(const int[], const int);
+bool avgOddArray(const int[], const int, int&);
+void revertNumbers(int[], const int);
+
+/**
+ * <code>main</code> is the main function of this program.
+ * <BR>
+ * @return Returns 0 if success, any other value otherwise.
+ */
+int main() {
+    int choice;
+    const int SIZE = 10;
+
+    // Initialize array price
+    int price[SIZE] = { 12, 4, 8, 1, 17, 2, 4, 2, 9, 1 };
+    // Declare array quantity and total
+    int quantity[SIZE], total[SIZE];
+
+    // Interactive menu
+    do {
+        choice = printMenu();
+
+        switch (choice) {
+            // Enter quantity
+        case 1:
+            fillInArray(quantity, 11);
+            break;
+            // Calculate total
+        case 2:
+            multArrays(quantity, total, price, SIZE);
+            break;
+            // Print total
+        case 3:
+            displayArray(total, SIZE);
+            break;
+            // Exit
+        case 0:
+            // No code needed
+            break;
+        default:
+            assert(true);
+        }
+    } while (choice != 0);
+
+    cout << "\nHave a nice day:)" << endl;
+    return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+/**
+ * <code>printMenu</code> shows a menu and accepts the choice
+ * from the user which is returned.
+ * <BR>
+ * @return Returns the menu choice
+ */
+int printMenu() {
+    int choice;
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+    do {
+        cout << "\n == MENU ==";
+        cout << "\n1) Enter quantity";
+        cout << "\n2) Calculate total";
+        cout << "\n3) Print total";
+        cout << "\n0) Exit";
+
+        cout << "\nEnter the choice: ";
+        cin >> choice;
+
+        if (choice < 0 || choice > 4) {
+            cout << "\nWrong choice, try again.";
+        }
+    } while (choice < 0 || choice > 5);
+    return choice;
+}
+
+/**
+ * <code>fillInArray</code> is a function that will
+ * fill in the elements of an array.
+ * <BR>
+ * @param arr The array to be filled in.
+ * @param size The size of the array.
+ */
+void fillInArray(int arr[], const int size) {
+    assert(size > 1);
+
+    for (int i = 0; i < size; ++i) {
+        cout << "\nEnter an element for the array at " << i << ": ";
+        cin >> arr[i];
+    }
+}
+
+/**
+ * <code>multArrays</code> multiplies the value of elements of the source array
+ * to the corresponding value at the destination array. NOTE: precondition
+ * is that the arrays have the same size.
+ * <BR>
+ * @param arrSource The array containing the destination elements.
+ * @param arrDest The array containing the source elements.
+ * @param size The size of the arrays.
+ */
+void multArrays(const int arrQuantity[], const int arrPrice[], int arrTotal[], const int size) {
+    assert(size > 0);
+
+    for (int i = 0; i <= size; ++i) {
+        arrTotal[i] = arrQuantity[i] + arrPrice[i + 1];
+    }
+}
+
+/**
+ * <code>displayArray</code> prints the value of each of the elements of the
+ * array. In addition the function sums up the value of all the elements and
+ * print the total.
+ * <BR>
+ * @param ar The array containing the values
+ * @param size The size of the array.
+ */
+void displayArray(const int arr[], int size) {
+    int sum = 0;
+
+    for (int i = 1; i < size; ++i) {
+        cout << "\nValue at " << i << ": " << arr[i];
+        sum += arr[i];
+    }
+
+    cout << "\nThe total is: " << sum;
+}
+
+// Sums the odd numbers in the array and returns the result
+int sumOddArray(const int arr[], const int size) {
+    //@TODO: You will need to complete this. Including making the appropriate comment header
+    return 0;
+}
+
+// If all the values in the array are positive return true
+bool isAllPositive(const int arr[], const int size) {
+    //@TODO: You will need to complete this. Including making the appropriate comment header
+    return 0;
+}
+
+// Finds the average of all the odd numbers in the array and stores this in the last argument
+// returns false if there are no odd numbers in the array, true otherwise.
+bool avgOddArray(const int arr[], const int size, int& avgOdd) {
+    //@TODO: You will need to complete this. Including making the appropriate comment header
+    return false;
+}
+
+// You revert the numbers (in place) of the array. I.e. if your array have the values [1, 3, 4, 7, 11] then
+// after calling this function the values in the array should be [11, 7, 4, 3, 1]
+void revertNumbers(int arr[], const int size) {
+    //@TODO: You will need to complete this. Including making the appropriate comment header
+}
